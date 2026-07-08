d3f771b5fb0ddec02d90dec961f800dbc93ac09a
BelalMuhamed <basalbelal25@gmail.com>
2026-07-08 22:39:23 +0300

    phasee04

--- Edvanz.Infrastructure/Persistence/EdvanzDbContext.cs
+++ Edvanz.Infrastructure/Persistence/EdvanzDbContext.cs
@@ -648,6 +648,8 @@ public class EdvanzDbContext(DbContextOptions<EdvanzDbContext> options) : DbCont
             entity.Property(ts => ts.StudentCode)
                 .HasMaxLength(10)
                 .IsRequired();
+            entity.HasIndex(ts => ts.StudentPhoneNumber).IsUnique();
+            entity.HasIndex(ts => ts.ParentPhoneNumber).IsUnique();
 
             // Composite unique: StudentCode is unique within each teacher's account
             entity.HasIndex(ts => new { ts.TeacherId, ts.StudentCode })
