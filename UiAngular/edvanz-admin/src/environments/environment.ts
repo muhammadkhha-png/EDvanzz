4bfe7eab6aaca044d926e7795efa92cd560a96b2
BelalMuhamed <basalbelal25@gmail.com>
2026-07-19 06:55:21 +0300

    remove front and seperate it

--- UiAngular/edvanz-admin/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-/** Production environment. Base URL is injected at build/deploy time. */
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://app-edvanz-api-prod.azurewebsites.net/api',
-};
